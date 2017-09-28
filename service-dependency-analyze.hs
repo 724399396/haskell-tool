2dcdd631e4ca059bf7012abaa2de7fb3fb9766b5
liwei <liwei72439936@gmail.com>
2017-09-29 00:13:10 +0800

    stash

--- service-dependency-analyze.hs
+++ service-dependency-analyze.hs
@@ -1,6 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 import System.Directory
 import Control.Monad
-import System.FilePath ((</>))
+import Data.Maybe
+import System.FilePath ((</>), takeExtension)
+import Data.Text as T hiding (map, filter)
+import qualified Data.Text.IO as TIO
 
 type FileFilter = FilePath -> IO Bool
 
@@ -14,3 +19,28 @@ getFiles fp = do isDirectory <- doesDirectoryExist fp
                             fss <- traverse getFiles $ map (fp </>) $ filter notSpecialEntrie fs
                             return (join fss))
                    else return [fp]
+
+type Namespace = Text
+type Class = Text
+type FullClass = (Namespace, Class)
+
+typeVarName :: FullClass -> FilePath -> IO (Maybe (FilePath, Class))
+typeVarName (ns, n) fp = do con <- TIO.readFile fp
+                            if (isInfixOf ns con && isInfixOf (append n " ") con)
+                              then (let
+                                      varName :: Maybe Text
+                                      varName = listToMaybe $ Prelude.dropWhile (/= "n") $ T.words con
+                                   in
+                                     return (fmap ((,) fp) varName))
+                              else return $ Nothing
+
+
+httpSendFind :: FilePath -> IO [FilePath]
+httpSendFind fp = do fs <- getFiles fp
+                     let
+                       csFs :: [FilePath]
+                       csFs = filter ((==) ".cs" . pack . takeExtension) fs
+                     fmap maybeToList <$> traverse (typeVarName ("Harness.Rest.Client", "IRestClient")) csFs
+
+main :: IO ()
+main = putStrLn ""
