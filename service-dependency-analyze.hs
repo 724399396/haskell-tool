803e8e4a311367ca4888b50d93d81a8c81f58193
liwei <weili@thoughtworks.com>
2017-09-29 14:18:33 +0800

    finish

--- service-dependency-analyze.hs
+++ service-dependency-analyze.hs
@@ -1,9 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import System.Directory
+import System.IO
 import Control.Monad
 import Data.Maybe
-import System.FilePath ((</>), takeExtension)
+import System.FilePath ((</>), takeExtension, splitDirectories)
 import Data.Text as T hiding (map, filter)
 import qualified Data.Text.IO as TIO
 
@@ -24,23 +25,33 @@ type Namespace = Text
 type Class = Text
 type FullClass = (Namespace, Class)
 
+readAsISO88591 :: FilePath -> IO Text
+readAsISO88591 fp = do h <- openFile fp ReadMode
+                       hSetEncoding h latin1
+                       TIO.hGetContents h
+
 typeVarName :: FullClass -> FilePath -> IO (Maybe (FilePath, Class))
-typeVarName (ns, n) fp = do con <- TIO.readFile fp
-                            if (isInfixOf ns con && isInfixOf (append n " ") con)
+typeVarName (ns, n) fp = do con <- readAsISO88591 fp
+                            if (isInfixOf ns con && isInfixOf n con)
                               then (let
                                       varName :: Maybe Text
-                                      varName = listToMaybe $ Prelude.dropWhile (/= "n") $ T.words con
+                                      varName = listToMaybe $ Prelude.dropWhile (/= n) $ T.words con
                                    in
                                      return (fmap ((,) fp) varName))
                               else return $ Nothing
 
+filePathToModuleAndFile :: FilePath -> (String, String, String)
+filePathToModuleAndFile fp = let ps = splitDirectories fp
+                             in
+                               (Prelude.head $ Prelude.drop 2 $ Prelude.dropWhile (/= "") ps, Prelude.head $ Prelude.drop 1 $ Prelude.reverse ps, Prelude.last ps)
 
-httpSendFind :: FilePath -> IO [FilePath]
+httpSendFind :: FilePath -> IO [(String,String, String)]
 httpSendFind fp = do fs <- getFiles fp
                      let
                        csFs :: [FilePath]
                        csFs = filter ((==) ".cs" . pack . takeExtension) fs
-                     fmap maybeToList <$> traverse (typeVarName ("Harness.Rest.Client", "IRestClient")) csFs
+                     fmap (filePathToModuleAndFile . fst) . join . fmap maybeToList <$> traverse (typeVarName ("Harness.Rest.Client", "IRestClient")) csFs
 
 main :: IO ()
-main = putStrLn ""
+main = do fs <- httpSendFind ""
+          mapM_ print fs
